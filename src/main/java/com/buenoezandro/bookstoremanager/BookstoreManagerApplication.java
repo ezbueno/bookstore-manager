9f20402410548b62b4661cec2c976f1123082b3c
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-03 22:04:21 -0300

    First commit: project structure created

---
+++ src/main/java/com/buenoezandro/bookstoremanager/BookstoreManagerApplication.java
@@ -0,0 +1,13 @@
+package com.buenoezandro.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bueno/ezandro/bookstoremanager/BookstoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bueno.ezandro.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
